5c94a1f365e894be5e8471d7df002165927e4a9b
norbiros <norbiros@protonmail.com>
2025-07-11 13:06:51 +0200

    refactor: merge platform-specific implementations into `EmojiTypeImpl`

--- neoforge/src/main/java/dev/norbiros/emojitype/neoforge/EmojiTypeImpl.java
+++ neoforge/src/main/java/dev/norbiros/emojitype/neoforge/EmojiTypeImpl.java
@@ -2,16 +2,27 @@ package dev.norbiros.emojitype.neoforge;
 
 import dev.norbiros.emojitype.EmojiType;
 import dev.norbiros.emojitype.config.EmojiTypeConfig;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.loading.FMLPaths;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 
+import java.nio.file.Path;
+
 @Mod(EmojiType.MOD_ID)
-public class EmojitypeNeoforge {
-    public EmojitypeNeoforge() {
+public class EmojiTypeImpl {
+    public EmojiTypeImpl() {
         EmojiType.init();
 
         ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class,
                 () -> (client, parent) -> EmojiTypeConfig.createConfigScreen(parent));
+    }
+
+    /**
+     * Implementation of {@link EmojiType#getConfigDirectory()}.
+     */
+    public static Path getConfigDirectory() {
+        return FMLPaths.CONFIGDIR.get();
     }
 }
